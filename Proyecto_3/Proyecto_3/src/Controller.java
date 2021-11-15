74bcdfc3be7d4ca541d473774fbc5e5ea7a2914f
Fantasma3005 <88512640+Fantasma3005@users.noreply.github.com>
2021-11-15 17:13:45 -0600

    Proyecto-03

---
+++ Proyecto_3/Proyecto_3/src/Controller.java
@@ -0,0 +1,150 @@
+import java.util.concurrent.TimeUnit;
+
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+
+
+public class Controller  {
+	
+	
+	public void Sonido1(ActionEvent event){
+		
+		Sonido sonido_1 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio1.wav");
+		Thread hilo = new Thread(sonido_1);
+		hilo.start();
+
+		}
+		
+
+	public void Sonido2(ActionEvent event){
+		
+		Sonido sonido_2 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio2.wav");
+		Thread hilo = new Thread(sonido_2);
+		hilo.start();
+
+	
+	}
+	public void Sonido3(ActionEvent event){
+		
+		Sonido sonido_3 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio3.wav");
+		Thread hilo = new Thread(sonido_3);
+		hilo.start();
+
+	
+	}
+	public void Sonido4(ActionEvent event){
+		
+		Sonido sonido_4 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio4.wav");
+		Thread hilo = new Thread(sonido_4);
+		hilo.start();
+
+	
+	}
+	public void Sonido5(ActionEvent event){
+		
+		Sonido sonido_5 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio5.wav");
+		Thread hilo = new Thread(sonido_5);
+		hilo.start();
+
+	
+	}
+	public void Sonido6(ActionEvent event){
+		
+		Sonido sonido_6 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio6.wav");
+		Thread hilo = new Thread(sonido_6);
+		hilo.start();
+
+	
+	}
+	public void Sonido7(ActionEvent event){
+		
+		Sonido sonido_7 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio7.wav");
+		Thread hilo = new Thread(sonido_7);
+		hilo.start();
+
+	
+	}
+	public void Sonido8(ActionEvent event){
+		
+		Sonido sonido_8 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio8.wav");
+		Thread hilo = new Thread(sonido_8);
+		hilo.start();
+
+	
+	}
+	public void Sonido9(ActionEvent event){
+		
+		Sonido sonido_9 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio9.wav");
+		Thread hilo = new Thread(sonido_9);
+		hilo.start();
+
+	
+	}
+	public void Sonido10(ActionEvent event){
+		
+		Sonido sonido_10 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio10.wav");
+		Thread hilo = new Thread(sonido_10);
+		hilo.start();
+
+	
+	}
+	public void Sonido11(ActionEvent event){
+		
+		Sonido sonido_11 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio11.wav");
+		Thread hilo = new Thread(sonido_11);
+		hilo.start();
+
+	
+	}
+	public void Sonido12(ActionEvent event){
+		
+		Sonido sonido_12 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio17.wav");
+		Thread hilo = new Thread(sonido_12);
+		hilo.start();
+
+	
+	}
+	public void Sonido13(ActionEvent event){
+		
+		Sonido sonido_13 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio13.wav");
+		Thread hilo = new Thread(sonido_13);
+		hilo.start();
+
+	
+	}
+	public void Sonido14(ActionEvent event){
+		
+		Sonido sonido_14 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio12.wav");
+		Thread hilo = new Thread(sonido_14);
+		hilo.start();
+
+	
+	}
+	public void Sonido15(ActionEvent event){
+		
+		Sonido sonido_15 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio15.wav");
+		Thread hilo = new Thread(sonido_15);
+		hilo.start();
+
+	
+	}
+	public void Sonido16(ActionEvent event){
+		
+		Sonido sonido_16 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio16.wav");
+		Thread hilo = new Thread(sonido_16);
+		hilo.start();
+
+	
+	}
+	public void Sonido17(ActionEvent event){
+		
+		Sonido sonido_17 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio17.wav");
+		Thread hilo = new Thread(sonido_17);
+		hilo.start();
+
+	
+	}
+}
---
+++ Proyecto_3/Proyecto_3/src/Main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		Parent root = FXMLLoader.load(getClass().getResource("dardos.fxml"));
+		primaryStage.setTitle("Musica");
+		Scene scene = new Scene(root, 600, 500);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
