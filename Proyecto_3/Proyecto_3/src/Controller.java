22094a67f31fd147656dd37d1d49eca1f4a442d6
Fantasma3005 <88512640+Fantasma3005@users.noreply.github.com>
2021-11-17 16:16:39 -0600

    Proyecto-03

--- Proyecto_3/Proyecto_3/src/Controller.java
+++ Proyecto_3/Proyecto_3/src/Controller.java
@@ -1,16 +1,17 @@
 import java.util.concurrent.TimeUnit;
-
 import javafx.event.ActionEvent;
-
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 
 
-public class Controller  {
+public class Controller {
 	
 	
-	public void Sonido1(ActionEvent event){
+	public void Sonido1(MouseEvent event){
 		
 		Sonido sonido_1 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio1.wav");
 		Thread hilo = new Thread(sonido_1);
@@ -147,4 +148,86 @@ public class Controller  {
 
 	
 	}
+	public void teclasPanel(KeyEvent evt){
+		if(evt.getCode()==KeyCode.Q) {
+			Sonido sonido_1 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio1.wav");
+			Thread hilo = new Thread(sonido_1);
+			hilo.start();
+		}
+		else if(evt.getCode()==KeyCode.W) {
+			Sonido sonido_2 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio2.wav");
+			Thread hilo = new Thread(sonido_2);
+			hilo.start();
+		}
+		else if(evt.getCode()==KeyCode.E) {
+			Sonido sonido_3 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio3.wav");
+			Thread hilo = new Thread(sonido_3);
+			hilo.start();
+		}
+		else if(evt.getCode()==KeyCode.R) {
+			Sonido sonido_4 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio4.wav");
+			Thread hilo = new Thread(sonido_4);
+			hilo.start();
+		}
+		else if(evt.getCode()==KeyCode.T) {
+			Sonido sonido_5 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio5.wav");
+			Thread hilo = new Thread(sonido_5);
+			hilo.start();
+		}
+		else if(evt.getCode()==KeyCode.Y) {
+			Sonido sonido_6 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio6.wav");
+			Thread hilo = new Thread(sonido_6);
+			hilo.start();
+		}
+		else if(evt.getCode()==KeyCode.U) {
+			Sonido sonido_7 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio7.wav");
+			Thread hilo = new Thread(sonido_7);
+			hilo.start();
+		}
+		else if(evt.getCode()==KeyCode.I) {
+			Sonido sonido_8 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio8.wav");
+			Thread hilo = new Thread(sonido_8);
+			hilo.start();
+		}
+		else if(evt.getCode()==KeyCode.O) {
+			Sonido sonido_9 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio9.wav");
+			Thread hilo = new Thread(sonido_9);
+			hilo.start();
+		}
+		else if(evt.getCode()==KeyCode.P) {
+			Sonido sonido_10 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio10.wav");
+			Thread hilo = new Thread(sonido_10);
+			hilo.start();
+		}
+		else if(evt.getCode()==KeyCode.A) {
+			Sonido sonido_11 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio11.wav");
+			Thread hilo = new Thread(sonido_11);
+			hilo.start();
+		}
+		else if(evt.getCode()==KeyCode.S) {
+			Sonido sonido_12 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio12.wav");
+			Thread hilo = new Thread(sonido_12);
+			hilo.start();
+		}
+		else if(evt.getCode()==KeyCode.D) {
+			Sonido sonido_13 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio13.wav");
+			Thread hilo = new Thread(sonido_13);
+			hilo.start();
+		}
+		else if(evt.getCode()==KeyCode.F) {
+			Sonido sonido_14 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio14.wav");
+			Thread hilo = new Thread(sonido_14);
+			hilo.start();
+		}
+		else if(evt.getCode()==KeyCode.G) {
+			Sonido sonido_15 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio15.wav");
+			Thread hilo = new Thread(sonido_15);
+			hilo.start();
+		}
+		else if(evt.getCode()==KeyCode.H) {
+			Sonido sonido_16 = new Sonido("platillo","C:\\Users\\Fantasma3005\\Downloads\\Proyecto_3\\Audios\\audio16.wav");
+			Thread hilo = new Thread(sonido_16);
+			hilo.start();
+		}
+}
 }
